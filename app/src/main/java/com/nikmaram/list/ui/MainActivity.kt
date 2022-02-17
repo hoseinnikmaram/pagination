1da8b1c4e2788c1a07a43628f360fa4dc5d0531c
nikmaram.mh <nikmaram@nilva.ir>
2022-02-17 21:22:03 +0330

    implement paging in MainActivity

--- app/src/main/java/com/nikmaram/list/ui/MainActivity.kt
+++ app/src/main/java/com/nikmaram/list/ui/MainActivity.kt
@@ -2,11 +2,32 @@ package com.nikmaram.list.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.nikmaram.list.R
+import com.nikmaram.list.databinding.ActivityMainBinding
+import com.nikmaram.list.util.RecyclerViewAdapter
+import kotlinx.coroutines.flow.collectLatest
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewModel: MainActivityViewModel
+    lateinit var binding: ActivityMainBinding
+    private lateinit var recyclerViewAdapter: RecyclerViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        recyclerViewAdapter = RecyclerViewAdapter{
+            Toast.makeText(this,it.toString(),Toast.LENGTH_SHORT).show()
+        }
+        binding.recycler.adapter = recyclerViewAdapter
+        viewModel  = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        lifecycleScope.launchWhenCreated {
+            viewModel.getListData().collectLatest {
+                recyclerViewAdapter.submitData(it)
+            }
+        }
     }
 }
