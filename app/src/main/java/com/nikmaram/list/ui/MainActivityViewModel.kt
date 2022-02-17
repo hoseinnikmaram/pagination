a175af720f1392b4c769fc247b22ef8b9abf28a5
nikmaram.mh <nikmaram@nilva.ir>
2022-02-17 12:38:13 +0330

    implement MainActivityViewModel.kt and get list data

---
+++ app/src/main/java/com/nikmaram/list/ui/MainActivityViewModel.kt
@@ -0,0 +1,21 @@
+package com.nikmaram.list.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.nikmaram.list.model.Photo
+import com.nikmaram.list.util.MAX_SIZE_PAGE
+import com.nikmaram.list.util.PAGE_SIZE
+import com.nikmaram.list.util.PhotoPagingSource
+import kotlinx.coroutines.flow.Flow
+
+class MainActivityViewModel : ViewModel() {
+
+    fun getListData(): Flow<PagingData<Photo>> {
+        return Pager (config = PagingConfig(pageSize = PAGE_SIZE, maxSize = MAX_SIZE_PAGE),
+            pagingSourceFactory = {PhotoPagingSource()}).flow.cachedIn(viewModelScope)
+    }
+}
--- app/src/main/java/com/nikmaram/list/util/Constants.kt
+++ app/src/main/java/com/nikmaram/list/util/Constants.kt
@@ -2,3 +2,4 @@ package com.nikmaram.list.util
 
 const val PAGE_SIZE = 10
 const val FIRST_PAGE_INDEX = 0
+const val MAX_SIZE_PAGE = 10
