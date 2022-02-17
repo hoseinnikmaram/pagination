bc6d94f8107e696bcfab68bd546d6685405f8e57
nikmaram.mh <nikmaram@nilva.ir>
2022-02-17 22:37:42 +0330

    refactor PhotoModel

--- app/src/main/java/com/nikmaram/list/model/PhotoModel.kt
+++ app/src/main/java/com/nikmaram/list/model/PhotoModel.kt
@@ -3,14 +3,14 @@ package com.nikmaram.list.model
 import com.nikmaram.list.R
 import com.nikmaram.list.util.PAGE_SIZE
 
-data class Photo(val image:Int,val id:Int)
+data class Photo(val image:Int,val id:Int,val page: Int)
 
 fun providePhotos(page:Int): MutableList<Photo> {
     val photoList = mutableListOf<Photo>()
     val startIndex = page * PAGE_SIZE
     val endIndex = startIndex + PAGE_SIZE
     for (i in startIndex..endIndex){
-        photoList.add(Photo(R.mipmap.ic_launcher_round,i))
+        photoList.add(Photo(R.mipmap.ic_launcher_round,i,page))
     }
     return photoList
 }
